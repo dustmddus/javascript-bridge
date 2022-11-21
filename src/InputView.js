94c6631717938d799b1b7848d042ef3a4e290842
dustmddus <dustmddus5@gmail.com>
2022-11-21 19:53:58 +0900

    feat: 다리 길이 입력, 다리 랜덤 값 생성

--- src/InputView.js
+++ src/InputView.js
@@ -1,16 +1,38 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const { Console } = require("@woowacourse/mission-utils");
+
 const InputView = {
+  validationLength(length) {
+    const lengthNum = Number(length);
+    if (Number.isNaN(lengthNum)) {
+      throw new Error("[ERROR]다리 길이는 숫자로 입력해주세요.");
+    }
+    if (lengthNum < 3 || lengthNum > 20) {
+      throw new Error("[ERROR]다리 길이는 3~20 사이 숫자여야 합니다.");
+    }
+  },
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine("다리의 길이를 입력해주세요.", (length) => {
+      try {
+        this.validationLength(length);
+        return length;
+      } catch (e) {
+        this.readBridgeSize();
+      }
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving() {
+    Console.readLine(
+      "이동할 칸을 선택해주세요. (위: U, 아래: D)",
+      (direction) => direction
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
